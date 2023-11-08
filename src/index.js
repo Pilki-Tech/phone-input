9453493937cf5ee443750620bd8bb03cdf56cd00
nimbleStalker <tarasowgeorgij@yandex.ru>
2023-11-08 23:23:58 +0300

    add build

--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ export { PhoneInput };
 
 export default {
   install(app, customOptions = {}) {
+    const {
+      dropdownOptions: customDropdownOptions,
+      inputOptions: customInputOptions,
+      ...otherCustomOptions
+    } = customOptions;
+    const {
+      dropdownOptions: defaultDropdownOptions,
+      inputOptions: defaultInputOptions,
+      ...otherDefaultOptions
+    } = defaultOptions;
+
+    utils.options = {
+      inputOptions: {
+        ...defaultInputOptions,
+        ...customInputOptions,
+      },
+      dropdownOptions: {
+        ...defaultDropdownOptions,
+        ...customDropdownOptions,
+      },
+      ...otherDefaultOptions,
+      ...otherCustomOptions,
+    };
     app.component('phone-input', PhoneInput);
   },
 }
